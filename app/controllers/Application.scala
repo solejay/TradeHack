1ff65eb6cb1e1ff85c34f1c88a574d52ec42eb2d
Clive Ayonye <clive078@gmail.com>
2012-11-30 16:48:00 +0100

    added methodology

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,16 +8,4 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index())
   }
-
-  def about = Action {
-    Ok(views.html.about())
-  }
-
-  def methodology = Action {
-    Ok(views.html.methodology())
-  }
-
-  def docs = Action {
-    Ok(views.html.docs())
-  }
 }
